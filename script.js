3c4167969813fe6803ed7ad487daa873ddfda045
TheYunDude <97200157+TheYunDude@users.noreply.github.com>
2023-02-22 13:05:04 -0800

    multiply feature

--- script.js
+++ script.js
@@ -5,24 +5,8 @@ let multiplierButton = document.getElementById("multiplier");
 let multiplierCheck = document.getElementById("multiplier");
 
 function up(){
-  count += 1;
-  counter.innerHTML = count;
-  if (multiplierCheck.innerHTML == "Multiplier: 2") {
-    count +=1;
-    counter.innerHTML = count;
-  }
-  if (multiplierCheck.innerHTML == "Multiplier: 3") {
-    count +=2;
-    counter.innerHTML = count;
-  }
-  if (multiplierCheck.innerHTML == "Multiplier: 4") {
-    count +=3;
-    counter.innerHTML = count;
-  }
-  if (multiplierCheck.innerHTML == "Multiplier: 5") {
-    count +=4;
-    counter.innerHTML = count;
-  }
+  count += multiplier
+  counter.innerHTML = count
 }
 
 function buyGrandma(){
