55b945906a0766f38c397c67653688f54b42c1a2
TheYunDude <97200157+TheYunDude@users.noreply.github.com>
2023-02-24 08:04:20 -0800

    saving and loading

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 let counter = document.getElementById("counter");
-let count = 0;
-let multiplier = 1;
 let multiplierButton = document.getElementById("multiplier");
 let multiplierCheck = document.getElementById("multiplier");
 
+let v = {
+  count: 0,
+  multiplier: 1
+};
+
 function up(){
-  count += multiplier
-  counter.innerHTML = count
+  v.count += v.multiplier;
+  counter.innerHTML = v.count;
 }
 
 function buyGrandma(){
-  if (count >= 30) {
-    count -= 30;
-    counter.innerHTML = count;
-    multiplier += 1;
-    multiplierButton.innerHTML = "Multiplier: " + multiplier;
+  if (v.count >= 30) {
+    v.count -= 30;
+    counter.innerHTML = v.count;
+    v.multiplier += 1;
+    multiplierButton.innerHTML = "Multiplier: " + v.multiplier;
   } else {
     alert("not enough cookies");
   }
+}
+
+function save(){
+  localStorage.setItem("variables", JSON.stringify(v));
+  // TRY TO SAVE THE GAME
+  try {
+    localStorage.setItem('variables',JSON.stringify(v));
+  }catch(err) {
+    console.log('Cannot access localStorage - browser may be old or storage may be corrupt')
+  }
+  console.log('Game saved successfully');
+}
+
+function load(){
+  v = JSON.parse(localStorage.getItem("variables"));
+
+  counter.innerHTML = v.count;
+  multiplierButton.innerHTML = "Multiplier: " + v.multiplier;
+  console.log(v);
 }
