902b4aa50b882ec82210ea604efa70d9085f40d8
TheYunDude <97200157+TheYunDude@users.noreply.github.com>
2023-02-25 11:16:53 -0800

    hire grandma to work for you

--- script.js
+++ script.js
@@ -4,24 +4,58 @@ let multiplierCheck = document.getElementById("multiplier");
 
 let v = {
   count: 0,
-  multiplier: 1
+  cps: 0,
+  grandmaPrice: 30,
+  slavePrice: 100,
+  cpc: 1
 };
 
 function up(){
-  v.count += v.multiplier;
+  v.count += v.cpc;
   counter.innerHTML = v.count;
 }
 
 function buyGrandma(){
-  if (v.count >= 30) {
-    v.count -= 30;
+  if (v.count >= v.grandmaPrice) {
+    v.count -= v.grandmaPrice;
     counter.innerHTML = v.count;
-    v.multiplier += 1;
-    multiplierButton.innerHTML = "Multiplier: " + v.multiplier;
+    v.cps++;
+    multiplierButton.innerHTML = "Cookies Per Second: " + v.cps;
+    v.grandmaPrice = Math.round(v.grandmaPrice * 1.5);
+    document.getElementById('grandmaPriceText').innerHTML = "Grandma Price: " + v.grandmaPrice;
   } else {
     alert("not enough cookies");
   }
 }
+
+function buySlave(){
+  if (v.count >= v.slavePrice) {
+    v.count -= v.slavePrice;
+    counter.innerHTML = v.count;
+    v.cps += 2;
+    v.cpc++;
+    multiplierButton.innerHTML = "Cookies Per Second: " + v.cps;
+    v.slavePrice = Math.round(v.slavePrice * 2);
+    document.getElementById('slavePriceText').innerHTML = "Slave Price: " + v.slavePrice;
+  } else {
+    alert("not enough cookies");
+  }
+}
+
+setInterval(function() {
+  v.count += v.cps;
+  counter.innerHTML = v.count;
+}, 1000)
+
+
+
+
+
+
+
+
+
+
 
 function save(){
   localStorage.setItem("variables", JSON.stringify(v));
@@ -38,6 +72,9 @@ function load(){
   v = JSON.parse(localStorage.getItem("variables"));
 
   counter.innerHTML = v.count;
-  multiplierButton.innerHTML = "Multiplier: " + v.multiplier;
+  multiplierButton.innerHTML = "Cookies Per Second: " + v.cps;
+  document.getElementById('grandmaPriceText').innerHTML = "Grandma Price: " + v.grandmaPrice;
+  document.getElementById('slavePriceText').innerHTML = "Slave Price: " + v.slavePrice;
+  
   console.log(v);
 }
